b146fb36ae001d56ae62c535fd2f2d8e98d779df
klushund <patrick.ritschel@gmx.at>
2023-01-13 12:05:06 +0100

    * Initial check-in

---
+++ leds_and_button/README.md
@@ -0,0 +1,19 @@
+leds_and_button app
+===================
+
+Beispiel des Buchs "Embedded Systems mit RISC-V", dpunkt.verlag
+
+Die Arbeitsweise ist in Abschnitt 5.5 Taster anschließen beschrieben.
+Im Beispiel werden zwei LEDs an GPIO4 und GPIO5 bei Betätigung des Tasters an GPIO6 geschaltet. 
+Das Beispiel verwendet die Funktionen des ESP-IDF für den Zugriff auf die Peripherie.
+
+Siehe auch das direkte Ansteuern der Peripherie in Beispiel switch_leds.
+
+Verwendet ein beliebiges Board mit ESP32-C3 Mikrocontroller.
+
+Siehe auch die [Webseite zum Buch](https://ritschel.at/buch-embedded-systems-auf-den-punkt-gebracht/).
+
+*The code of this project is in the Public Domain (or CC0 licensed, at your option).
+Unless required by applicable law or agreed to in writing, this
+software is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR
+CONDITIONS OF ANY KIND, either express or implied.*
---
+++ leds_and_button/main/main.c
@@ -0,0 +1,60 @@
+/*
+ * Example of the book "Embedded Systems mit RISC-V", dpunkt.verlag
+ * Author: Patrick Ritschel
+ *
+ * see https://ritschel.at/kapitel-5-5-taster-anschliessen-beispiel-leds_and_button/
+ *
+ * Based on the basic Espressif IDF-project.
+ *
+ * The code of this project is in the Public Domain (or CC0 licensed, at your option).
+ * Unless required by applicable law or agreed to in writing, this
+ * software is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR
+ * CONDITIONS OF ANY KIND, either express or implied.
+ */
+#include <stdio.h>
+#include <stdbool.h>
+#include <unistd.h>
+#include "driver/gpio.h"
+#include "freertos/FreeRTOS.h"
+#include "freertos/task.h"
+
+#define LED1_GPIO						GPIO_NUM_4
+#define LED2_GPIO						GPIO_NUM_5
+#define BTN_GPIO						GPIO_NUM_6
+
+// ***** implementation *****
+void app_main() {
+	printf("Two LEDs and a button via ESP-IDF demo\n");
+	// initialize GPIOs, with implicit multiplexing
+	gpio_config_t gpioConfig = {
+		.pin_bit_mask = (1 << LED1_GPIO) | (1 << LED2_GPIO),
+		.mode = GPIO_MODE_OUTPUT,
+		.pull_up_en = false,
+		.pull_down_en = false,
+		.intr_type = GPIO_INTR_DISABLE
+	};
+	gpio_config(&gpioConfig);
+	gpio_config_t gpioConfigIn = {
+		.pin_bit_mask = (1 << BTN_GPIO),
+		.mode = GPIO_MODE_INPUT,
+		.pull_up_en = true,
+		.pull_down_en = false,
+		.intr_type = GPIO_INTR_DISABLE
+	};
+	gpio_config(&gpioConfigIn);
+
+	uint32_t ledLevel = 0;
+	uint32_t btnActive = 0;
+	while (true) {
+		if (gpio_get_level(BTN_GPIO) == 0) { // pressed
+			if (++btnActive == 2) { // pressed long enough
+				ledLevel = !ledLevel;
+				gpio_set_level(LED1_GPIO, ledLevel);
+				gpio_set_level(LED2_GPIO, ledLevel);
+			}
+		} else {
+			btnActive = 0;
+		}
+		vTaskDelay(50 / portTICK_PERIOD_MS);
+	}
+}
